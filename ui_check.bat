75c238caf7947c9059030d3566570ae917cba318
ImmoSS <83536444+ImmoSS@users.noreply.github.com>
2022-11-27 22:58:46 +0300

    Update ui_check.bat

--- ui_check.bat
+++ ui_check.bat
@@ -3,66 +3,64 @@ pushd "%~dp0"
 cd ..
 set patchfolder=Tournament Mod V5.1a
 ECHO Y | del "%cd%\%patchfolder%\UI\"
-ECHO -------------------------------------------------
+REM -------------------------------------------------
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\CultureOverview.lua" "%cd%\%patchfolder%\UI\CultureOverview.lua"
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\CultureOverview.xml" "%cd%\%patchfolder%\UI\CultureOverview.xml"
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\EnemyUnitPanel.lua" "%cd%\%patchfolder%\UI\EnemyUnitPanel.lua"
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\JoiningRoom.lua" "%cd%\%patchfolder%\UI\JoiningRoom.lua"
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\VictoryProgress.xml" "%cd%\%patchfolder%\UI\VictoryProgress.xml"
 ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\MPList.lua" "%cd%\%patchfolder%\UI\MPList.lua"
-ECHO -------------------------------------------------
-REM проверяет наличие файла в папке интерфейса, если файл есть ничего не делает, если нету помещает его копию в tm/ui
+REM -------------------------------------------------
 IF EXIST "%cd%\UI_bc1\UnitFlagManager\UnitFlagManager.lua" (
   ECHO UnitFlagManager.lua exists on EUI, skipping
 ) ELSE (
   ECHO UnitFlagManager.lua does not exists on EUI, copying to TM
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\UnitFlagManager.lua" "%cd%\%patchfolder%\UI\UnitFlagManager.lua"
 )
-ECHO -------------------------------------------------
+REM -------------------------------------------------
 IF EXIST "%cd%\UI_bc1\PlotHelp\PlotHelpManager.lua" (
   ECHO PlotHelpManager.lua exists on EUI, skipping
 ) ELSE (
   ECHO PlotHelpManager.lua does not exists on EUI, copying to TM
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\PlotHelpManager.lua" "%cd%\%patchfolder%\UI\PlotHelpManager.lua"
 )
-ECHO -------------------------------------------------
+REM -------------------------------------------------
 IF EXIST "%cd%\UI_bc1\PlotHelp\PlotHelpManager.xml" (
   ECHO PlotHelpManager.xml exists on EUI, skipping
 ) ELSE (
   ECHO PlotHelpManager.xml does not exists on EUI, copying to TM
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\PlotHelpManager.xml" "%cd%\%patchfolder%\UI\PlotHelpManager.xml"
 )
-ECHO -------------------------------------------------
+REM -------------------------------------------------
 IF EXIST "%cd%\UI_bc1\CityView\ProductionPopup.lua" (
   ECHO ProductionPopup.lua exists on EUI, skipping
 ) ELSE (
   ECHO ProductionPopup.lua does not exists on EUI, copying to TM
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\ProductionPopup.lua" "%cd%\%patchfolder%\UI\ProductionPopup.lua"
 )
-ECHO -------------------------------------------------
-REM менять текст поиска под каждый файл
+REM -------------------------------------------------
 set text="-- modified by bc1 from 1.0.3.144 brave new world code"
 FIND %text% "%cd%\UI_bc1\CityStatePopup\CityStateDiploPopup.lua" && (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\eui\CityStateDiploPopup.lua" "%cd%\%patchfolder%\UI\CityStateDiploPopup.lua"
 ) || (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\CityStateDiploPopup.lua" "%cd%\%patchfolder%\UI\CityStateDiploPopup.lua"
 )
-ECHO -------------------------------------------------
+REM -------------------------------------------------
 set text="-- coded by bc1 from 1.0.3.276 brave new world code"
 FIND %text% "%cd%\UI_bc1\CityView\CityView.lua" && (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\eui\CityView.lua" "%cd%\%patchfolder%\UI\CityView.lua"
 ) || (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\CityView.lua" "%cd%\%patchfolder%\UI\CityView.lua"
 )
-ECHO -------------------------------------------------
+REM -------------------------------------------------
 set text="-- coded by bc1 from Civ V 1.0.3.276 code"
 FIND %text% "%cd%\UI_bc1\TopPanel\TopPanel.lua" && (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\eui\TopPanel.lua" "%cd%\%patchfolder%\UI\TopPanel.lua"
 ) || (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\ui\TopPanel.lua" "%cd%\%patchfolder%\UI\TopPanel.lua"
 )
-ECHO -------------------------------------------------
-set text="if popupInfo.Data1 == -2 then"
+REM -------------------------------------------------
+set text="Game.SelectionListGameNetMessage( GameMessageTypes.GAMEMESSAGE_DO_COMMAND, action.CommandType, action.CommandData, -1, 0, bAlt );"
 FIND %text% "%cd%\UI_bc1\Improvements\ConfirmCommandPopup.lua" && (
   ECHO F | xcopy /s /y "%cd%\%patchfolder%\tmp\eui\ConfirmCommandPopup.lua" "%cd%\%patchfolder%\UI\ConfirmCommandPopup.lua"
 ) || (
